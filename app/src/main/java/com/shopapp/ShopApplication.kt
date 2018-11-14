acaefc60d2a15666d4736a6ff5cba6d2a9d5c55f
@Venkata.Udatha2 <venkata.udatha2@cvshealth.com>
2018-11-14 17:19:28 +0530

    * Added code to get all variants

--- app/src/main/java/com/shopapp/ShopApplication.kt
+++ app/src/main/java/com/shopapp/ShopApplication.kt
@@ -11,6 +11,7 @@ import com.shopapp.data.dao.impl.DaoImpl
 import com.shopapp.di.component.AppComponent
 import com.shopapp.di.component.DaggerAppComponent
 import com.shopapp.di.module.RepositoryModule
+import com.shopapp.shopify.api.ShopifyApi
 import com.shopapp.gateway.Api
 import io.fabric.sdk.android.Fabric
 import io.reactivex.plugins.RxJavaPlugins
@@ -29,7 +30,11 @@ open class ShopApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val api = null //Initialize your api here.
+
+        val api = ShopifyApi(this, "porganicworld.myshopify.com",
+                "62761bc137e3dac4c66d1d1a93d8dab3",
+                "1e5f1c6facb67ef0bf9e0c4af642d192",
+                "5c3061bb2e053642fc1422409ccbb89e") ; //null //Initialize your api here.
         val dao = DaoImpl(this)
 
         appComponent = buildAppComponent(api, dao)
